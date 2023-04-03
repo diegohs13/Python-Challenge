6fab204252397619d8a44870fae91fefc4d6fea1
diegohs13 <diego.hsoliveira13@gmail.com>
2023-04-03 15:13:16 -0300

    Second commit

--- main.py
+++ main.py
@@ -48,48 +48,69 @@ while True:
     cpf_formatado = cpf_sem_barra.replace('.', '')
 
     if cpf_formatado.isnumeric():
-
         if len(cpf_formatado) == 11:
-
             if cpf_formatado in blocklist:
-
                 print('O CPF não pode conter todos números iguais!\n')
-
             else:
                 if validador_cpf(cpf_formatado):
                     print('Seu CPF está valido!\n')
+                    print('Olá! Seu atendimento será iniciado!')
+
                     while True:
-                        print('Olá! Seu atendimento será iniciado!')
-
                         carroceria = input("Por favor digíte o tipo de carroceria do seu veículo:\n")
-
-                        if carroceria:
-
-                            altura = input(
-                                'Para continuar, digite a altura do veiculo em centimetros ou "X" para sair:\n')
-
-                            if altura.isnumeric():
-                                print(altura)
-
-                            elif altura.lower() == 'x':
-                                break
-
-                            else:
-                                print('Digite a altura do veiculo ou "X" para sair!\n')
-
-                        elif carroceria.lower() == 'x':
-                            break
+                        if carroceria.isspace():
+                            print('Digite o tipo de carroceria do veiculo!\n')
                         else:
-                            print('Digite o tipo de carroceria do veiculo ou "X" para sair!\n')
+                            while True:
+                                altura = input('Para continuar, digite'
+                                               ' a altura do veiculo em centimetros:\n')
+
+                                if altura.isnumeric():
+                                    while True:
+                                        comprimento = input('Digite o comprimento do veiculo em centimetros:\n')
+
+                                        if comprimento.isnumeric():
+                                            while True:
+                                                chassi = input('Para continuar, digite o chassi do veiculo:\n')
+
+                                                if chassi.isspace():
+                                                    print('Por favor, digite o chassi!')
+
+                                                else:
+                                                    while True:
+
+                                                        print(f'Olá, verifique se suas informações estão corretas:\n'
+                                                              f'CPF: {cpf_formatado}\n'
+                                                              f'Comprimento: {comprimento}\n'
+                                                              f'Altura: {altura}\n'
+                                                              f'Chassi: {chassi}\n'
+                                                              f'Carroceria: {carroceria}\n'
+                                                              f'Se estão corretas digite "sim" , se não '
+                                                              f'digite qualquer outra coisa: ')
+                                                        digito_verificador = input()
+
+                                                        if digito_verificador.lower() == 'sim':
+                                                            print('Sua solicitação foi feita!')
+                                                            exit()
+                                                        else:
+                                                            print('Ok, digite as informações novamente!')
+                                                            exit()
+
+                                        elif comprimento.isspace():
+                                            print('Por favor, digite o comprimento!')
+
+                                        else:
+                                            print('O comprimento deve ser digitado em numeros!')
+                                elif altura.isspace():
+                                    print("Por favor, utilize numeros!")
+                                else:
+                                    print('A altura deve ser utilizada apenas numeros!')
                 else:
                     print('Seu CPF está invalido\n')
-
         else:
             print('O CPF deve conter 11 dígitos!\n')
-
     elif cpf_formatado.lower() == 'x':
         break
-
     else:
         print('Digite seu CPF corretamente ou apenas "x" para sair!\n')
 
