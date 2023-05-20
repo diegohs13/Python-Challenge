de36ae4c009635b0d2461c8d2ae088dd5c3ff20b
diegohs13 <diego.hsoliveira13@gmail.com>
2023-05-20 12:58:46 -0300

    SEGUNDA ENTREGA

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print('-' * 20)
+import time
+
+print('-' * 100)
 print('Bem vindo a Porto!')
 print('-' * 100)
 
@@ -32,17 +34,19 @@ def validador_cpf(cpf):
 
 
 def abertura_de_sinistro():
-    print('-' * 100)
-    print('Olá, o acionamento do seguro será feito por aqui!')
-    print('-' * 100)
 
     if cpf_formatado in segurados:
 
         while True:
+            print('-' * 100)
+            print('Olá, o acionamento do seguro será feito por aqui!')
+            print('-' * 100)
+
             print(f'Olá {segurados[cpf_formatado]["Nome"]} Verificamos que sua apólice é de'
                   f' {segurados[cpf_formatado]["Apólice"]}\n')
             print('Por favor, nos diga o que aconteceu!')
             print('[0] Batida\n[1] Pane elétrica\n[2] Desatres naturais\n[3] Sair')
+
             escolha_usuario = input('Selecione uma das opções acima:')
 
             if escolha_usuario.isnumeric():
@@ -57,20 +61,26 @@ def abertura_de_sinistro():
                           f'Comprimento: {segurados[cpf_formatado]["Comprimento"]}\n'
                           f'Peso: {segurados[cpf_formatado]["Peso"]}\n'
                           f'Largura: {segurados[cpf_formatado]["Largura"]}\n')
-                    digito_verificador = input('[1] Sim\n[2] Não\nSuas informações estão corretas?:')
 
-                    while escolha_usuario:
+                    while True:
+                        digito_verificador = input('[1] Sim\n[2] Não\nSuas informações estão corretas?:')
                         if digito_verificador.isnumeric():
                             if digito_verificador == '1':
                                 return '0'
 
                             elif digito_verificador == '2':
-                                print('Por favor entre em contato com o nosso SAC para alterar sua apólice')
+                                print('-' * 100)
+                                print('Por favor entre em contato com o nosso SAC para alterar sua apólice\n'
+                                      'SAC: 11946236792')
+                                print('-' * 100)
                                 return ''
 
                             else:
+                                print('-' * 100)
                                 print('Por favor escolha somente as opções informadas!')
+
                         else:
+                            print('¨' * 100)
                             print('Por favor utilize números na hora da escolha!')
 
                 if escolha_usuario == '1':
@@ -84,20 +94,26 @@ def abertura_de_sinistro():
                           f'Comprimento: {segurados[cpf_formatado]["Comprimento"]}\n'
                           f'Peso: {segurados[cpf_formatado]["Peso"]}\n'
                           f'Largura: {segurados[cpf_formatado]["Largura"]}\n')
-                    digito_verificador = input('[1] Sim\n[2] Não\nSuas informações estão corretas?:')
 
-                    while escolha_usuario:
+                    while True:
+                        digito_verificador = input('[1] Sim\n[2] Não\nSuas informações estão corretas?:')
                         if digito_verificador.isnumeric():
                             if digito_verificador == '1':
                                 return '1'
 
                             elif digito_verificador == '2':
-                                print('Por favor entre em contato com o nosso SAC para alterar sua apólice')
+                                print('-' * 100)
+                                print('Por favor entre em contato com o nosso SAC para alterar sua apólice\n'
+                                      'SAC: 11946236792')
+                                print('-' * 100)
                                 return ''
 
                             else:
+                                print('-' * 100)
                                 print('Por favor escolha somente as opções informadas!')
+
                         else:
+                            print('¨' * 100)
                             print('Por favor utilize números na hora da escolha!')
 
                 if escolha_usuario == '2':
@@ -111,37 +127,54 @@ def abertura_de_sinistro():
                           f'Comprimento: {segurados[cpf_formatado]["Comprimento"]}\n'
                           f'Peso: {segurados[cpf_formatado]["Peso"]}\n'
                           f'Largura: {segurados[cpf_formatado]["Largura"]}\n')
-                    digito_verificador = input('[1] Sim\n[2] Não\nSuas informações estão corretas?:')
 
                     while escolha_usuario:
+                        digito_verificador = input('[1] Sim\n[2] Não\nSuas informações estão corretas?:')
                         if digito_verificador.isnumeric():
                             if digito_verificador == '1':
                                 return '2'
 
                             elif digito_verificador == '2':
-                                print('Por favor entre em contato com o nosso SAC para alterar sua apólice')
+                                print('-' * 100)
+                                print('Por favor entre em contato com o nosso SAC para alterar sua apólice\n'
+                                      'SAC: 11946236792')
+                                print('-' * 100)
                                 return ''
 
                             else:
+                                print('-' * 100)
                                 print('Por favor escolha somente as opções informadas!')
+
                         else:
+                            print('¨' * 100)
                             print('Por favor utilize números na hora da escolha!')
 
                 elif escolha_usuario == '3':
-                    print(f'A opção escolhida foi [{escolha_usuario}] Sair\n')
+                    print('-' * 100)
+                    print('Você escolheu sair!')
+                    print('-' * 100)
                     return ''
 
                 else:
+                    print('-' * 100)
                     print('Por favor escolha apenas as opções listadas acima!')
+                    print('-' * 100)
 
             else:
+                print('-' * 100)
                 print('Por favor utilize números na hora da escolha!')
+                print('-' * 100)
 
     else:
-        print('Você não tem nenhuma apolice cadastrada!')
+        print('-' * 100)
+        print('Você não tem nenhuma apolice cadastrada!\n'
+              'Para cadastrar uma apolice, por favor entre no nosso site: https://www.portoseguro.com.br/')
+        print('-' * 100)
+        return ''
 
 
 def solicitar_guincho(sinistro):
+    print('-' * 100)
     print('Pediremos apenas mais algumas informações para concluir com a solicitação do guincho.')
     print('-' * 100 + '\n')
 
@@ -151,7 +184,6 @@ def solicitar_guincho(sinistro):
             print('-' * 100)
 
             if veiculo_tombado.isnumeric():
-
                 while True:
                     if veiculo_tombado == '1':
                         peso_carga_entrada = input('Digite o peso da carga que está sendo transportada em toneladas: ')
@@ -160,45 +192,34 @@ def solicitar_guincho(sinistro):
                         if peso_carga_entrada.isnumeric():
                             peso_carga = float(peso_carga_entrada)
 
-                            if peso_carga <= 3.0:
-                                print('Guincho para cargas de até 3t e com suporte para vaiculos capotados')
-                                return ''
-
-                            elif peso_carga >= 3.1:
-                                print('Guincho para cargas com mais de 3t até 6t e com suporte para vaiculos capotados')
-                                return ''
-
-                            elif peso_carga >= 6.0:
-                                print('Guincho para cargas com mais de 6t e com suporte para vaiculos capotados')
-                                return ''
+                            print(f'Ok! Estamos enviando um guincho para veículos tombados com cargas de '
+                                  f'{peso_carga} toneladas')
+                            print('-' * 100)
+                            return ''
 
                         else:
                             print('Por favor utilize numeros para digitar o peso da carga')
 
                     elif veiculo_tombado == '2':
                         peso_carga_entrada = input('Digite o peso da carga que está sendo transportada em toneladas: ')
+                        print('-' * 100)
 
                         if peso_carga_entrada.isnumeric():
                             peso_carga = float(peso_carga_entrada)
 
-                            if peso_carga <= 3.0:
-                                print('Guincho para cargas de até 3t')
-                                return ''
-
-                            elif peso_carga >= 3.1:
-                                print('Guincho para cargas com mais de 3t e até 6t')
-                                return ''
-
-                            elif peso_carga >= 6.0:
-                                print('Guincho para cargas com mais de 6t')
-                                return ''
+                            print(f'Ok! Estamos enviando um guincho para veículos cargas de '
+                                  f'{peso_carga} toneladas')
+                            print('-' * 100)
+                            return ''
 
                         else:
                             print('Por favor utilize numeros para digitar o peso da carga')
 
                     else:
+                        print('-' * 100)
                         print('Por favor escolha somente as opções dadas!')
-
+                        print('-' * 100)
+                        break
             else:
                 print('Por favor digite numeros na hora da escolha\n')
 
@@ -211,22 +232,69 @@ def solicitar_guincho(sinistro):
             if bateria.isnumeric():
                 while True:
                     if bateria == '1':
+                        print('-' * 100)
                         print('Ok! Estamos enviando um motoboy com uma bateria comum para a troca!')
+                        print('-' * 100)
                         return ''
 
                     elif bateria == '2':
+                        print('-' * 100)
                         print('Ok! Estamos enviando um motoboy com uma bateria de caminhões para a troca! ')
+                        print('-' * 100)
                         return ''
                     else:
+                        print('-' * 100)
                         print('Por favor escolha somente as opções dadas!')
-
+                        print('-' * 100)
+                        break
             else:
+                print('-' * 100)
                 print('Por favor utilize numeros na hora da escolha')
+                print('-' * 100)
 
     elif sinistro == 2:
-        print('oi')
+        while True:
+            desastres_naturais = input('[1] Árvore caída\n[2] Desmoronamento\n[3] Alagamento\n[4] Raio\n'
+                                       'Por favor escolha uma das opções de acordo com o ocorrido:')
+
+            if desastres_naturais.isnumeric():
+                while True:
+                    if desastres_naturais == '1':
+                        print('-' * 100)
+                        print('Ok! Estamos enviando um guincho para retirada de veículos atingido por árvores!')
+                        print('-' * 100)
+                        return ''
+
+                    elif desastres_naturais == '2':
+                        print('-' * 100)
+                        print('Ok! Estamos enviando um guincho para veículos atingidos por desmoronamento!')
+                        print('-' * 100)
+                        return ''
+
+                    elif desastres_naturais == '3':
+                        print('-' * 100)
+                        print('Ok! Estamos enviando um guincho para veículos alagados!')
+                        print('-' * 100)
+                        return ''
+
+                    elif desastres_naturais == '4':
+                        print('-' * 100)
+                        print('Ok! Estamos enviando um guincho para seu veículo atingido!')
+                        print('-' * 100)
+                        return ''
+
+                    else:
+                        print('-' * 100)
+                        print('Por favor escolha somente as opções dadas!')
+                        print('-' * 100)
+                        break
+            else:
+                print('-' * 100)
+                print('Por favor utilize numeros na hora da escolha')
+                print('-' * 100)
     else:
-        print('deu bug')
+        print('Não entendemos sua solicitação, por favor entre em contato com nosso SAC\n'
+              'SAC: 11946236792')
 
 
 blocklist = [
@@ -271,8 +339,6 @@ while True:
             else:
                 if validador_cpf(cpf_formatado):
                     print('-' * 100)
-                    print('Seu CPF está valido!')
-                    print('-' * 100)
                     print('x' * 100)
 
                     tipo_sinistro_entrada = abertura_de_sinistro()
@@ -282,19 +348,28 @@ while True:
 
                         tipo_guincho = solicitar_guincho(tipo_sinistro)
                         print(tipo_guincho)
+                        print('Em 10 segundos o atendimento será encerrado!')
+                        time.sleep(10)
                         break
 
                     else:
-                        print('Você escolheu sair!')
+                        print('Em 10 segundos o atendimento será encerrado')
+                        time.sleep(10)
                         break
 
                 else:
                     print('Seu CPF está invalido\n')
+
         else:
             print('O CPF deve conter 11 dígitos!\n')
+
     elif cpf_formatado.lower() == 'x':
+        print('Em 10 segundos o atendimento será encerrado')
+        time.sleep(10)
         break
     else:
         print('Digite seu CPF corretamente ou apenas "x" para sair!\n')
 
+print('x' * 100)
 print('Atendimento encerrado!')
+print('x' * 100)
